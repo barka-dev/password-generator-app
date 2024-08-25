d1e7e941ce397ad7f6e24d4d08447eccecc14c51
Abdel <abdelbarka96@gmail.com>
2024-08-25 19:15:56 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const range = document.querySelector('input[type="range"]');
 const label_for_check = document.querySelectorAll('.label_for_check');
+const char_length_display = document.querySelector('#char_length_display');
+const copy_icon = document.querySelector('#copy_icon');
+const copy_msg = document.querySelector('#copy_msg');
 
 const toggleCheck = (e) => {
     e.target.classList.toggle('checked');
@@ -16,6 +19,20 @@ const handleRangeColor = (e) => {
     range.style.background = `linear-gradient(90deg, rgba(164,255,175,1) 0%, rgba(164,255,175,1) ${position}%, rgba(24,23,31,1) ${position}%)`;
 }
 
+
 range.addEventListener('input', (e)=>{
     handleRangeColor(e);
-})
+    char_length_display.textContent = e.target.value;
+});
+
+const showCopyMsg = () =>{
+    copy_msg.classList.add('show_msg');
+    setTimeout(()=>{
+        copy_msg.classList.remove('show_msg');
+    }, 1000);
+}
+
+copy_icon.addEventListener('click', ()=>{
+    showCopyMsg();
+    
+});
