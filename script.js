9020a002d2b2c5fb95c0914bd8bbd9f4ed7a28f1
Abdel <abdelbarka96@gmail.com>
2024-08-25 15:28:14 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,17 @@
 const range = document.querySelector('input[type="range"]');
+const label_for_check = document.querySelectorAll('.label_for_check');
+
+const toggleCheck = (e) => {
+    e.target.classList.toggle('checked');
+}
+
+label_for_check.forEach((label)=>{
+    label.addEventListener('click', (e)=>{
+        toggleCheck(e);
+    })
+})
 
 const handleRangeColor = (e) => {
-    console.log("position: ", e.target.value, e.target.max);
     const position = (e.target.value*100)/e.target.max;
     range.style.background = `linear-gradient(90deg, rgba(164,255,175,1) 0%, rgba(164,255,175,1) ${position}%, rgba(24,23,31,1) ${position}%)`;
 }
